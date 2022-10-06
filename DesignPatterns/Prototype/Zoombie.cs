fa8087361b1cd008ef7026036839517a1d9b8139
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-06 15:25:51 +0600

    Prototype #3

---
+++ DesignPatterns/Prototype/Zoombie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Zoombie : ICloneable
+    {
+        public string Look { get; set; }
+        public double Strength { get; set; }
+        public double Health { get; set; }
+        public double Speed { get; set; }        
+
+        public Zoombie Copy()
+        {
+            return new Zoombie()
+            {
+                Look = Look,
+                Strength = Strength,
+                Health = Health,
+                Speed = Speed,
+            };
+        }
+
+        public object Clone()
+        {
+            return new Zoombie();
+        }
+    }
+}
