adae9a1033d9a8a596f5cdc265242f6081341c31
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-06 12:45:48 +0600

    Singleton #1

---
+++ DesignPatterns/Singleton/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger? _logger;
+
+        private Logger()
+        {
+
+        }
+
+        public static Logger GetLogger()
+        {
+            if(_logger is null)
+                _logger = new Logger();
+            
+            return _logger;
+        }
+
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
