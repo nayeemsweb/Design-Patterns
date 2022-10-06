2002dff541f82e86877c8db6f98f6f98ee6a254f
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-06 16:15:35 +0600

    StringBuilder #5

--- DesignPatterns/Builder/Program.cs
+++ DesignPatterns/Builder/Program.cs
@@ -1,5 +1,13 @@
-﻿var builder = new CarBuilder();
+﻿using System.Text;
+
+var builder = new CarBuilder();
 builder.AddEngine()
     .AddColor("White")
     .AddDoor()
     .Build();
+
+//StringBuilder follows Builder Pattern
+var stringBuilder = new StringBuilder();
+var message = stringBuilder.Append("Hello ")
+    .Append("Nayeem").ToString();
+Console.WriteLine(message);
