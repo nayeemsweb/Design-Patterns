e3342fd67ec28ad5187dcaa6aba53031b808ee3e
Nayeem Rahman <nayeem12rahman@gmail.com>
2022-10-06 18:48:39 +0600

    AbstractFactory #8

---
+++ DesignPatterns/AbstractFactory/AmericanMilitaryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class AmericanMilitaryFactory : MilitaryFactory
+    {
+        public override Fighter GetFighter()
+        {
+            return new F16();
+        }
+
+        public override Ship GetShip()
+        {
+            return new Carrier();
+        }
+    }
+}
---
+++ DesignPatterns/AbstractFactory/RussianMilitaryFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class RussianMilitaryFactory : MilitaryFactory
+    {
+        public override Fighter GetFighter()
+        {
+            return new Mig29();
+        }
+
+        public override Ship GetShip()
+        {
+            return new Submarine();
+        }
+    }
+}
