8e2d2fd4f6ac074bcb8cdc12f4b064032a167648
Nayeem Rahman <nayeem12rahman@gmail.com>
2023-01-04 12:24:10 +0600

    Strategy Pattern Tested well #12

--- DesignPatterns/Strategy/Program.cs
+++ DesignPatterns/Strategy/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Strategy;
+
+var shoppingBag1 = new ShoppingMall(new LowDiscountStrategy())
+{
+    CustomerName = "Rafi Samnan",
+    BillingAmmount = 1000
+};
+
+var shoppingBag2 = new ShoppingMall(new HighDiscountStrategy())
+{
+    CustomerName = "Manik Hossain",
+    BillingAmmount = 1000
+};
+
+var shoppingBag3 = new ShoppingMall(new NoDiscountStrategy())
+{
+    CustomerName = "Nayeem Rahman",
+    BillingAmmount = 1000
+};
+
+Console.WriteLine(shoppingBag1.GetBillingAmmount());
+Console.WriteLine(shoppingBag2.GetBillingAmmount());
+Console.WriteLine(shoppingBag3.GetBillingAmmount());
