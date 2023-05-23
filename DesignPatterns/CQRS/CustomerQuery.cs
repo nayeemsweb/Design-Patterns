84b06d50a14e16d6df4fd3bcf9704443484eb6d5
Nayeem Rahman <nayeem12rahman@gmail.com>
2023-05-23 06:22:55 +0600

    CQRS added

---
+++ DesignPatterns/CQRS/CustomerQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS
+{
+    public class CustomerQuery
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public decimal Billamount { get; set; }
+
+
+        public int NumberOfVisits { get; set; }
+        public string Behaviour { get; set; }
+    }
+}
---
+++ DesignPatterns/CQRS/Program.cs
@@ -0,0 +1,10 @@
+﻿using CQRS;
+
+var repo = new Repository();
+
+var customer = new CustomerCommand();
+customer.FirstName = "Nayeem";
+
+repo.Save(customer);
+
+List<CustomerQuery> customers = repo.Read();
